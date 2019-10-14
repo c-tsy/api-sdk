b5eaa2db397ee6323a08e202b5bd01a4b9fcad3f
castleiMac <castle@tansuyun.cn>
2019-10-14 20:50:11 +0800

    初始化

--- READMD.md
+++ READMD.md
@@ -2,18 +2,13 @@ api-sdk
 
 # 碳素云Api云服务 nodejs-sdk
 ```typescript
-ApiConfig.AppID = "你的AppID";
-ApiConfig.Secret = "你的Secret经过MD5加密后的内容";
-ApiConfig.UserToken = "当前操作人的用户Token，";
-const a = new IM.One();
-(async () => {
-    try {
-        let rs = await a.send("接收人的用户ID","内容" )
-        let rs = await a.read(1, 10, '接收人的用户ID')
-        console.log(JSON.stringify(rs))
-        debugger
-    } catch (error) {
-        debugger
-    }
-})()
+import CreateApi from '@ctsy/api-sdk';
+import hook, { HookWhen } from '@ctsy/hook';
+
+const api = CreateApi('appid','secret',usertoken)
+hook.regist('im/recv/'+usertoken,HookWhen.After,'recvmsg',(rpc,data)=>{
+    //rcp为推送对象
+    //data为数据对象
+    console.log(data)
+})
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Controller } from '@ctsy/request';
+import Client from '@ctsy/ws-rpc-client'
+import { base_covert } from '@ctsy/covert';
+import hook, { HookWhen } from '@ctsy/hook';
 const md5: any = require('md5')
-export default class ApiConfig {
+class ApiConfig {
     static AppID: string = "";
     static Secret: string = "";
     static UserToken: string = "";
@@ -69,9 +72,42 @@ interface MsgItem {
      */
     Text: string;
 }
+export var ApiWSClient: Client | undefined;
 
+/**
+ * 创建Api客户端
+ * @param appid 
+ * @param secret 
+ * @param usertoken 
+ */
+export default function create(appid: string, secret: string, usertoken: string) {
+    ApiConfig.AppID = appid;
+    ApiConfig.Secret = secret;
+    if (usertoken) {
+        ApiConfig.UserToken = usertoken;
+        ApiWSClient = new Client('wss://https://v1.api.tansuyun.cn/ws', base_covert(10, 62, Math.floor(Math.random() * 1000000)).toString())
+        ApiWSClient.subscribe('im/recv/' + usertoken, (data, rpc) => {
+            hook.emit('im/recv/' + usertoken, HookWhen.After, rpc, data);
+        })
+    }
+    return {
+        IM: {
+            One: new IM.One(),
+            Group: new IM.Group()
+        }
+    }
+}
+/**
+ * 
+ */
 export class ApiController extends Controller {
     host = "https://v1.api.tansuyun.cn/"
+    /**
+     * 发起请求
+     * @param method 
+     * @param data 
+     * @param opt 
+     */
     async _post(method: string, data: any, opt?: any) {
         let headers = {
             appid: ApiConfig.AppID,
@@ -85,7 +121,34 @@ export class ApiController extends Controller {
         });
     }
 }
-export namespace IM {
+namespace IM {
+    export class Group extends ApiController {
+        prefix = '_im';
+        constructor() {
+            super('Group')
+        }
+        /**
+         * 发送消息
+         * @param To 接收者 
+         * @param Text 内容
+         * @param CType 内容类型
+         * @param Device 设备类型
+         * @param Addr 地址
+         * @param Files 附件列表
+         */
+        async send(To: string, Text: string, CType: string = "text", Device: string = "unknow", Addr: string = '', Files: string[] = []) {
+            return await this._post('send', { To, Text, CType, Device, Addr, Files })
+        }
+        /**
+         * 读取消息记录
+         * @param P 页码
+         * @param N 每页数量
+         * @param To 接收者
+         */
+        async read(P: number = 1, N: number = 10, To?: string): Promise<MsgReadSearchResult> {
+            return await this._post('read', { P, N, To })
+        }
+    }
     export class One extends ApiController {
         prefix = '_im';
         constructor() {
