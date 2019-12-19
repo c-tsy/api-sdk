6a4d9d279a4e75147f9fe674df45ad0c512d8fa6
ida <ida@idaMacBook-Pro.lan>
2019-12-19 11:35:51 +0800

    [+] 补充User中的所有接口

--- src/modules/User.ts
+++ src/modules/User.ts
@@ -1,8 +1,100 @@
 import { ApiController, ApiConfig } from '../';
 import hook, { HookWhen } from '@ctsy/hook';
+import { ErrorType } from '../lib';
 const md5: any = require('md5')
 export namespace User {
     const prefix = "_user"
+    export class AuthObject {
+        /**
+         * 密码Hash盐
+         */
+        Salt: string = ""
+    
+        /**
+         * 字段定义
+         */
+        Fields = {
+            /**
+             * 账号
+             */
+            Account: 'Account',
+            /**
+             * 密码
+             */
+            PWD: 'PWD',
+            /**
+             * 原始密码字段
+             */
+            OldPWD: 'OldPWD',
+            /**
+             * 验证码
+             */
+            VCode: 'VCode',
+            /**
+             * 验证字段
+             */
+            VAccount: 'VAccount',
+            /**
+             * 权限字段名称
+             */
+            Permission: 'Permission',
+            /**
+             * 用户编号
+             */
+            UID: 'UID',
+            /**
+             * 推介人
+             */
+            PUID: 'PUID',
+            /**
+             * 第二种验证模式的验证字段
+             */
+            Verify: 'Verify'
+        }
+    
+        Limit = {
+            /**
+             * 注册时必须有推介人？
+             */
+            RegistMustPUID: false,
+    
+            RegistMustVCode: false,
+    
+            VerifyVAccount: false,
+        }
+    
+        /**
+         * 错误提示信息
+         */
+        Errors = {
+            E_NO_PUID: '无推介人',
+            E_PARAMS: '参数错误',
+            E_ACCOUNT_NOT_EXIST: '账号不存在',
+            E_PWD_EMPTY: '密码不存在',
+            E_PWD_ERROR: '密码错误',
+            E_NOT_LOGIN: '未登录',
+            E_PARAMS_FAILD: '参数验证失败',
+            E_VCODE: '验证码错误或已过期',
+            E_REG_ERROR: '注册失败',
+            E_ACCOUNT_USED: '账号已被使用',
+            E_CERTIFICATED: '账户已认证',
+            E_CERTIFICATING: '请等待认证结果后再申请',
+            E_PUID_NOT_EXIST: '推介人不存在',
+            E_NO_CERTIFICATION: '无该认证信息',
+            E_JUDGED: '该认证已被处理',
+            E_ACCOUNT_FORBIDDEN: '账户已被禁用',
+            E_ACCOUNT_ERROR: '账号不符合规则',
+            E_ACCOUNT_EXISTED: '账号已存在',
+            E_ACCOUNT_BINDDED: "该账号已绑定"
+        }
+        Verify = {
+            Account: /^[\w\b_-]{5,}$/,
+            PWD: /.{6,}/
+        }
+    
+        Hook: { [index: string]: any } = {
+        }
+    }
     class group extends ApiController {
         constructor() {
             super('Group', prefix);
@@ -12,6 +104,9 @@ export namespace User {
          * @param Type 
          */
         all(Type: string = 'all') {
+            if(!['list','tree','all'].includes(Type)) {
+                throw new Error(ErrorType.User.TYPE_PARAMS_IS_ERROR)
+            }
             return this.get('all', { Type })
         }
         /**
@@ -92,11 +187,31 @@ export namespace User {
             super('Auth', prefix);
         }
         /**
+         * 验证账号验证码
+         * @param data 
+         * @param remove 
+         */
+        vcheck(data: any, remove: boolean = true) {
+            return this.post('vcheck',{data, remove})
+        }
+        vcode(data: any, expire: number | any = 0) {
+            if(expire.appid) {
+                return ''
+            }
+            return this.post('vcode', {data, expire})
+        }
+        /**
          * 账号密码登陆
          * @param Account 
          * @param PWD 
          */
         async login(Account: string, PWD: string) {
+            if('string' != typeof Account) {
+                throw new Error(ErrorType.User.ACCOUNT_SHOULD_BE_STRING)
+            }
+            if('string' != typeof PWD) {
+                throw new Error(ErrorType.User.PWD_SHOULD_BE_STRING)
+            }
             let rs = await this.post('login', { Account, PWD: md5(PWD) })
             if (rs.UID) {
                 hook.emit('logined', HookWhen.After, '', rs);
@@ -125,7 +240,7 @@ export namespace User {
          * @param Account 
          */
         async thirdUnBind(Type: string, Account: string) {
-            return await this.post('abind', { Type, Account });
+            return await this.post('aunbind', { Type, Account });
         }
         /**
          * 获取我的权限
@@ -165,6 +280,18 @@ export namespace User {
          * @param PWD 
          */
         reset(OldPWD: string, PWD: string) {
+            if('string' != typeof PWD) {
+                throw new Error(ErrorType.User.PWD_SHOULD_BE_STRING)
+            }
+            if(/.{6,}/.test(PWD)) {
+                throw new Error(ErrorType.User.PWD_PARAMS_IS_ERROR)
+            }
+            if('string' != typeof OldPWD) {
+                throw new Error(ErrorType.User.OLDPWD_SHOULD_BE_STRING)
+            }
+            if(/.{6,}/.test(OldPWD)) {
+                throw new Error(ErrorType.User.OLDPWD_PARAMS_IS_ERROR)
+            }
             return this.post('reset', { OldPWD: md5(OldPWD), PWD: md5(PWD) })
         }
         /**
@@ -174,6 +301,18 @@ export namespace User {
          * @param PUID 
          */
         async regist(Name: string, Nick: string, Account: string, PWD: string, Sex: number = 1, PUID: number = 0) {
+            if(Name == '' || Nick == '') {
+                throw new Error(ErrorType.User.NAME_OR_NICK_CANNOT_BE_EMPTY)
+            }
+            if('string' != typeof Account) {
+                throw new Error(ErrorType.User.ACCOUNT_SHOULD_BE_STRING)
+            }
+            if('string' != typeof PWD) {
+                throw new Error(ErrorType.User.PWD_SHOULD_BE_STRING)
+            }
+            if(/.{6,}/.test(PWD)) {
+                throw new Error(ErrorType.User.PWD_PARAMS_IS_ERROR)
+            }
             return await this.post('regist', { Name, Nick, Sex, Account, PWD: md5(PWD), PUID })
         }
         /**
@@ -183,7 +322,26 @@ export namespace User {
          * @param VCode 
          */
         forget(Account: string, PWD: string, VCode: string) {
+            if('string' != typeof PWD) {
+                throw new Error(ErrorType.User.PWD_SHOULD_BE_STRING)
+            }
+            if(/.{6,}/.test(PWD)) {
+                throw new Error(ErrorType.User.PWD_PARAMS_IS_ERROR)
+            }
+            if('string' != typeof Account) {
+                throw new Error(ErrorType.User.ACCOUNT_SHOULD_BE_STRING)
+            }
+            if(/^[\w\b_-]{5,}$/.test(Account)) {
+                throw new Error(ErrorType.User.ACCOUNT_PARAMS_IS_ERROR)
+            }
             return this.post('forget', { Account, PWD, VCode });
+        }
+        /**
+         * 重写session
+         * @param UID 
+         */
+        rsession(UID: any) {
+            return this.post('rsession',{ UID })
         }
     }
     export const Auth = new auth();
