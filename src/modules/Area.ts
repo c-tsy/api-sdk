087b8c1f50d0a31fe494336fb8fbf7fc95a0d48a
castleiMac <castle@tansuyun.cn>
2020-02-18 20:46:09 +0800

    小区和楼栋数据结构

--- src/modules/Area.ts
+++ src/modules/Area.ts
@@ -6,11 +6,97 @@ namespace AreaApi {
         Name: string = "";
         Code: number = 0;
     }
+    export class Community {
+
+        /**
+         * 小区编号
+         * 
+         */
+        public CID: number = 0;
+        /**
+         * 行政代码
+         * 
+         */
+        public Code: number = 0;
+        /**
+         * 地址
+         * 
+         */
+        public Addr: string = "";
+        /**
+         * 名称
+         * 
+         */
+        public Title: string = "";
+        /**
+         * 竣工时间
+         * 
+         */
+        public FTime: Date = new Date;
+        /**
+         * 负责人
+         * 
+         */
+        public Name: string = "";
+        /**
+         * 创建时间
+         * 
+         */
+        public CTime: Date = new Date;
+        /**
+         * 创建人
+         * 
+         */
+        public CUID: number = 0;
+    }
+    export class Building {
+
+        /**
+         * 楼栋编号
+         * 
+         */
+        public BID: number = 0;
+        /**
+         * 小区编号
+         * 
+         */
+        public CID: number = 0;
+        /**
+         * 楼栋号
+         * 
+         */
+        public BNo: string = '';
+        /**
+         * 单元数
+         * 
+         */
+        public UnitAmount: number = 1;
+        /**
+         * 电梯数
+         * 
+         */
+        public LiftAmount: number = 0;
+        /**
+         * 每层户数
+         * 
+         */
+        public HouseAmount: number = 1;
+        /**
+         * 创建时间
+         * 
+         */
+        public CTime: Date = new Date;
+        /**
+         * 创建人
+         * 
+         */
+        public CUID: number = 0;
+    }
     class area extends ApiController {
-        read(PCode: number) {
+        read(PCode: number): Promise<{ Level: number, Rows: Area[] }> {
             return this.post('read', { PCode });
         }
-        save(Code: number, Name: number, Level: number) {
+        save(Code: number, Name: number, Level: number): Promise<boolean> {
             return this.post('save', { Code, Name, Level })
         }
     }
