4a9b757d6689da6b9854e410000cfa62fbaee48e
castleiMac <castle@tansuyun.cn>
2020-02-21 10:26:29 +0800

    发送微信推送

--- src/modules/Wechat.ts
+++ src/modules/Wechat.ts
@@ -1,14 +1,33 @@
 import { ApiController } from '../index';
 namespace Wechat {
-    class auth extends ApiController {
+    class WechatController extends ApiController {
+        protected get_url(method: string) {
+            if (!WechatID) {
+                throw new Error('WechatID Error')
+            }
+            return ['', this.prefix, this.name, method, WechatID].join('/');
+        }
     }
-    class admin extends ApiController { }
-    class js extends ApiController { }
-    class menu extends ApiController { }
+    class auth extends WechatController {
+    }
+    class admin extends WechatController { }
+    class js extends WechatController { }
+    class menu extends WechatController { }
+    class msg extends WechatController {
+        send(TemplateID: string, Data: { OpenID: string, Data: { [index: string]: string }[] }) {
+            this.post('send', { TemplateID, Data });
+        }
+    }
     const prefix = '_wechat'
+    export var WechatID = "";
+    export function set_wechatid(id: string) {
+        WechatID = id;
+        return true;
+    }
     export const Auth = new auth('Auth', prefix);
     export const Admin = new admin('Admin', prefix);
     export const Js = new js('Js', prefix);
     export const Menu = new menu('Menu', prefix);
+    export const MsgApi = new msg('Menu', prefix);
 }
 export default Wechat;
