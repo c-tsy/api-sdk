e9ab33dda16f06ba5ce3f74ec4dc41d64f99a9ac
castleiMac <castle@tansuyun.cn>
2020-02-25 16:40:10 +0800

    微信模板消息接口

--- src/modules/Wechat.ts
+++ src/modules/Wechat.ts
@@ -13,9 +13,45 @@ namespace Wechat {
     class admin extends WechatController { }
     class js extends WechatController { }
     class menu extends WechatController { }
+    class MsgType {
+        value: string = ''
+        color: string = ''
+    }
     class msg extends WechatController {
-        send(TemplateID: string, Data: { OpenID: string, Data: { [index: string]: string }[] }) {
-            this.post('send', { TemplateID, Data });
+        send(TemplateID: string, Data: { OpenID: string, Data: { first: MsgType | string, remark: MsgType | string, [index: string]: MsgType | string } }[]) {
+            if (Data instanceof Array && Data.length > 0) {
+                for (let x of Data) {
+                    if (x.OpenID && x.OpenID.length > 10) {
+
+                    } else {
+                        throw new Error('接收对象参数错误')
+                    }
+                    if (!x.Data.first) {
+                        throw new Error('发送对象参数错误')
+                    }
+                    for (let s of Object.keys(x.Data)) {
+                        if ('string' == typeof x.Data[s]) {
+                            // if (x.Data[s].length <= 0)
+                            //     throw new Error('发送内容为空')
+                            // x.Data[s].
+                        }
+                        switch (s) {
+                            case 'first': break;
+                            case 'remark': break;
+                            default: break;
+                        }
+                    }
+                    if ('string' == typeof x.Data.first && x.Data.first.length > 0) {
+                        x.Data.first = { value: x.Data.first, color: '' }
+                    } else { throw new Error('First 参数错误') }
+                    if ('string' == typeof x.Data.remark && x.Data.remark.length > 0) {
+                        x.Data.remark = { value: x.Data.remark, color: '' }
+                    } else { throw new Error('First 参数错误') }
+
+                }
+                return this.post('send', { TemplateID, Data });
+            }
+            throw new Error('消息内容应该为数组且长度大于0')
         }
     }
     const prefix = '_wechat'
