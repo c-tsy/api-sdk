a5e3705dcd687707e3b6efe8ab9c124e1e6b455c
castleiMac <castle@tansuyun.cn>
2020-03-10 23:26:58 +0800

    Excel导出支持通过table标签定义结构

--- src/modules/Data.ts
+++ src/modules/Data.ts
@@ -34,9 +34,27 @@ namespace DataApi {
         }
     }
     class excel extends ApiController {
-        save(File: string, Data: { [index: string]: any[] }): Promise<{ Token: string }> {
-            return this._post('save', { File, Data })
+        /**
+         * 通过数据结构来生成数据
+         * @param File 
+         * @param Data 
+         */
+        async save(File: string, Data: { [index: string]: any[] }): Promise<{ Token: string }> {
+            // if (window && window.navigator.platform == "")
+            return await this._post('save', { File, Data })
         }
+        /**
+         * 通过构建的Table来生成Excel
+         * @param File 
+         * @param Table 
+         */
+        async table(File: string, Table: string): Promise<{ Token: string }> {
+            return await this._post('table', { File, Table })
+        }
+        /**
+         * 发起下载请求
+         * @param Token 
+         */
         down(Token: string) {
             window.open([ApiConfig.Host, p, 'Excel', 'down', Token].join('/'))
         }
