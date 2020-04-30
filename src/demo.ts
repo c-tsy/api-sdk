df19941f9e3e7aa560b2703085f8e05423abfa43
castleiMac <castle@tansuyun.cn>
2020-04-30 15:53:00 +0800

    task demo

--- src/demo.ts
+++ src/demo.ts
@@ -5,3 +5,8 @@ import ClassTask from './modules/task/class/Task';
 let task = new ClassTask();
 task.Title = 'aba'
 TaskApi.TaskApi.add(task)
+TaskApi.TaskApi.search({
+    W: {},
+    Keyword: '',
+    P: 1, N: 10,
+});
--- src/modules/Task.ts
+++ src/modules/Task.ts
@@ -1,38 +1,21 @@
-import { ApiController } from "..";
+import { ApiController, ControllerApi } from "..";
 import comment from "./task/class/Comment";
 import ClassTask from "./task/class/Task";
 
 namespace TaskApi {
     let prefix = '_task';
 
-    class task extends ApiController {
-        /**
-         * 添加任务信息
-         */
-        async adds(Tasks: ClassTask[]) {
-            return this._post('adds', Tasks);
-        }
-        /**
-         * 添加单个
-         * @param Task 
-         */
-        async add(Task: ClassTask) {
-            return this._post('adds', [Task]);
-        }
-        /**
-         * 修改后保存任务信息
-         */
-        async save() { }
-        /**
-         * 读取任务列表
-         */
-        async list() { }
-        /**
-         * 任务操作，如指派，完成，暂停，继续等
-         */
-        async op() { }
+    class task extends ControllerApi {
     }
     export const TaskApi = new task('Task', prefix);
+
+    class project extends ControllerApi {
+    }
+    export const ProjectApi = new project('Project', prefix);
+
+    class tag extends ControllerApi {
+    }
+    export const TagApi = new tag('Tag', prefix);
 }
 
 export default TaskApi;
